1de2f4d8c0ed66a47a13e2d6eea8244bbf66a58e
Mathias Vigouroux <56303941+MathiasVigouroux@users.noreply.github.com>
2023-04-18 14:11:05 +0200

    putting back the old one

--- pet-master/pet/tasks.py
+++ pet-master/pet/tasks.py
@@ -24,9 +24,7 @@ from typing import List, Dict, Callable
 
 import log
 from pet import task_helpers
-from pet.task_helpers import MultiMaskTaskHelper
 from pet.utils import InputExample
-
 
 logger = log.get_logger('root')
 
@@ -928,10 +926,9 @@ class AllocineProcessor(DataProcessor):
 
 # register the processor for this task with its name
 #PROCESSORS[AllocineProcessor.TASK_NAME] = AllocineProcessor
-#from pet.tasks import TASK_HELPERS
-#from pet.task_helpers import MultiMaskTaskHelper
+
 # optional: if you have to use verbalizers that correspond to multiple tokens, uncomment the following line
-#TASK_HELPERS['allocine'] = MultiMaskTaskHelper
+# TASK_HELPERS[MyTaskDataProcessor.TASK_NAME] = MultiMaskTaskHelper
     
     
 
